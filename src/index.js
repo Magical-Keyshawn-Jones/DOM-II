932721bddacb71db16c340ef19855f2101c8e319
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-02-08 12:32:04 -0600

    I understand everything

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// On load
+
+window.onload = function () {
+    
+}
+
+const top = document.getElementsByTagName('header');
+    console.log(top)
+    top[0].addEventListener ('click', (event) => {event.target.style.backgroundColor = 'blue'})
